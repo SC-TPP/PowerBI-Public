32431759b1b2f15bc39ec7a1419cc07e573ffe1f
Andrew Lowson <lowsona@stirling.gov.uk>
2020-08-21 13:22:33 +0100

    added if clause to handle null values

--- Calculated Columns/Convert HTML.m
+++ Calculated Columns/Convert HTML.m
@@ -1,14 +1,17 @@
 //Add the code below to the calculated column dialogue box to convert HTML to plaintext (converting special characters such as &amp and removing html tags like <br>)
 //Replace [HTML Column] with the Correct column reference for your scenario
-let
-    GetPlainTextAsTable = 
-                            Html.Table(
-                                        [HTML Column],
-                                            {
-                                                {"Plaintext",":root"}
-                                            }
-                                    ),
-    //Expand the table created above down to only select the "Plaintext" column and then only the first row value (there are no other values returned due to ":root")
-    GetPlaintext = GetPlainTextAsTable[Plaintext]{0}
-in 
-    GetPlaintext
+if [HTML Column] = null
+    then null
+    else 
+        let
+            GetPlainTextAsTable = 
+                Html.Table(
+                    [HTML Column],
+                        {
+                            {"Plaintext",":root"}
+                        }
+                ),
+            //Expand the table created above down to only select the "Plaintext" column and then only the first row value (there are no other values returned due to ":root")
+            GetPlaintext = GetPlainTextAsTable[Plaintext]{0}
+        in 
+            GetPlaintext
